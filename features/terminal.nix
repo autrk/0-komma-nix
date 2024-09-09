4dcaecf111fe25982e896c6935c8dd776d0a0784
Christian Kihm-Landau <57010+autrk@users.noreply.github.com>
2024-09-09 23:41:29 +0000

    add terminal feature

---
+++ features/terminal.nix
@@ -0,0 +1,26 @@
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "docker-compose" "docker" ];
+      theme = "dst";
+    };
+    initExtra = ''
+      bindkey '^f' autosuggest-accept
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
--- home.nix
+++ home.nix
@@ -2,10 +2,16 @@
   home.username =  "autrk";
   home.homeDirectory = "/home/autrk";
   home.stateVersion = "24.05";
+ 
+  # Let Home Manager install and manage itself. 
   programs.home-manager.enable = true;
 
   home.packages = [
     pkgs.tree
     pkgs.neovim
+  ];
+
+  imports = [
+    ./features/terminal.nix
   ];
 }
